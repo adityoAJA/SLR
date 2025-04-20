4689a86b209c77f412e1c5953f3ed6eac69f018c
adityoAJA <168532434+adityoAJA@users.noreply.github.com>
2025-04-20 15:17:57 +0700

    Update 2_Proyeksi_Tinggi_Muka_Laut.py

--- pages/2_Proyeksi_Tinggi_Muka_Laut.py
+++ pages/2_Proyeksi_Tinggi_Muka_Laut.py
@@ -25,7 +25,7 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Fungsi untuk mengunduh dan mengekstrak file ZIP dari GitHub Release
-# @st.cache_resource
+@st.cache_resource
 def download_and_extract_zip(url, output_folder):
     if not os.path.exists(output_folder) or not os.listdir(output_folder):
         os.makedirs(output_folder, exist_ok=True)
